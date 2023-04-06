0cb3dfc6f222e752c61f4d5d273f7cf9b485f0bf
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-04-06 09:11:36 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,13 +2,19 @@ const container = document.querySelector(".container")
 const blok = document.querySelectorAll(".blok");
 const point = document.querySelectorAll(".point");
 const image = document.querySelectorAll(".background__image");
-const kraj = document.querySelectorAll(".country__description");
-const miasto = document.querySelectorAll(".city__description");
-const from = document.querySelectorAll(".from__text");
-const cena = document.querySelectorAll(".price__text");
+const krajopis = document.querySelectorAll(".country__description");
+const miastoopis = document.querySelectorAll(".city__description");
+const fromtext = document.querySelectorAll(".from__text");
+const cenatext = document.querySelectorAll(".price__text");
+const kraj = document.querySelectorAll(".country");
+const miasto = document.querySelectorAll(".city");
+const cena = document.querySelectorAll(".price");
+const from = document.querySelectorAll(".from");
+
 let slideIndex = 0;
 let timer = null;
 let timer2 = null;
+let timer3 = null;
 
 fetch("https://rekrutacja.webdeveloper.rtbhouse.net/files/banner_vip.json")
   .then((response) => {
@@ -33,10 +39,10 @@ fetch("https://rekrutacja.webdeveloper.rtbhouse.net/files/banner_vip.json")
       const priceText = data.offers[randIndex].priceText;
       image[i].src = imageUrl;
       image[i].alt = city;
-      kraj[i].innerHTML = country;
-      miasto[i].innerHTML = city;
-      from[i].innerHTML = priceText;
-      cena[i].innerHTML = price + " " + currency;
+      krajopis[i].innerHTML = country;
+      miastoopis[i].innerHTML = city;
+      fromtext[i].innerHTML = priceText;
+      cenatext[i].innerHTML = price + " " + currency;
     }
   });
 
@@ -51,18 +57,39 @@ function changeSlide() {
   for (let i = 0; i < blok.length; i++) {
     point[i].className = point[i].className.replace(" active", "");
   }
-  blok[slideIndex - 1].style.display = "block";
+  blok[slideIndex - 1].style.display = "flex";
   point[slideIndex - 1].className += " active";
-  timer = setTimeout(changeSlide, 5000);
+  timer = setTimeout(changeSlide, 4800);
 }
 changeSlide();
 
-// function elo() {
-//   for (let i = 0; i < image.length; i++) {
-//     if (image[i].classList.contains("fade")) {
-//       image[i].classList.remove("fade");
-//     }
-//   }
-// }
+function elo() {
+  for (let i = 0; i < image.length; i++) {
+    if (image[i].classList.contains("fade")) {
+      image[i].classList.remove("fade");
+    }
+    image[i].classList.add("scalefade");
+  }
+}
 
-// timer2 = setTimeout(elo, 2800);
+timer2 = setTimeout(elo, 1200);
+
+
+function reset() {
+  for (let i = 0; i < image.length; i++) {
+    kraj[i].style.animationDelay = "1300ms";
+    kraj[i].style.animationDuration = "3500ms";
+    krajopis[i].style.animationDelay = "1500ms";
+    krajopis[i].style.animationDuration = "3300ms";
+    miasto[i].style.animationDelay = "1500ms";
+    miasto[i].style.animationDuration = "3300ms";
+    miastoopis[i].style.animationDelay = "1700ms";
+    miastoopis[i].style.animationDuration = "3100ms";
+    cenatext[i].style.animationDelay = "1500ms";
+    cenatext[i].style.animationDuration = "3300ms";
+    from[i].style.animationDelay = "1500ms";
+    from[i].style.animationDuration = "3300ms";
+  }
+}
+
+timer3 = setTimeout(reset, 4000);
